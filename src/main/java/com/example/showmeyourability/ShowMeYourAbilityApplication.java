e6cd9cb9dd4eb1ebb541af3dd11fdda8df37f3b9
안승현 <>
2023-01-20 21:52:14 +0900

    first commit

---
+++ src/main/java/com/example/showmeyourability/ShowMeYourAbilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.showmeyourability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeYourAbilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowMeYourAbilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/showmeyourability/ShowMeYourAbilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showmeyourability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeYourAbilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
