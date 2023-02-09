4f474c90802d038e2b70965485898f97bdcd93ed
smilejakdu <ash982416@gmail.com>
2023-02-10 01:19:32 +0900

    find recent teacher

--- src/main/java/com/example/showmeyourability/teacher/TeacherController.java
+++ src/main/java/com/example/showmeyourability/teacher/TeacherController.java
@@ -1,6 +1,8 @@
 package com.example.showmeyourability.teacher;
 
+import com.example.showmeyourability.teacher.application.FindRecentTeacherApplication;
 import com.example.showmeyourability.teacher.application.FindTeacherApplication;
+import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.FindRecentTeacherResponseDto;
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.FindTeacherByIdResponseDto;
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.FindTeacherResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TeacherController {
     private final FindTeacherApplication findTeacherApplication;
+
+    private final FindRecentTeacherApplication findRecentTeacherApplication;
 
     @GetMapping()
     public FindTeacherResponseDto findAllTeacher(
@@ -25,5 +29,10 @@ public class TeacherController {
             @PathVariable Long teacherId
     ) {
         return findTeacherApplication.findOneTeacherById(teacherId);
+    }
+
+    @GetMapping("/recentTeacher")
+    public FindRecentTeacherResponseDto findRecentTeacher() {
+        return findRecentTeacherApplication.execute();
     }
 }
--- src/main/java/com/example/showmeyourability/teacher/infrastructure/repository/TeacherRepository.java
+++ src/main/java/com/example/showmeyourability/teacher/infrastructure/repository/TeacherRepository.java
@@ -2,8 +2,14 @@ package com.example.showmeyourability.teacher.infrastructure.repository;
 
 import com.example.showmeyourability.teacher.domain.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    @Query("SELECT t FROM Teacher t WHERE t.createdAt >= ?1")
+    List<Teacher> findByCreatedAtAfter(LocalDateTime dateTime);
 }
