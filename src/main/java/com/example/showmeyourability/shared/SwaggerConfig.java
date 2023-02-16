7dd2f3f54d3cfbfc0edbb1f082a41f9d1cefa83a
smilejakdu <ash982416@gmail.com>
2023-02-17 06:37:46 +0900

    spring boot swagger

---
+++ src/main/java/com/example/showmeyourability/shared/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.example.showmeyourability.shared;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        description = "Local server",
+                        url = "http://localhost:12425"
+                )
+        }
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi storeOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("stores")
+                .packagesToScan("com.example.showmeyourability.store.controller")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("users")
+                .packagesToScan("com.example.showmeyourability.user.controller")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("SpringBoot Rest API Documentation")
+                .description("Spring Boot를 이용한 Rest API 문서입니다.")
+                .version("0.1")
+                .termsOfService("http://localhost:12425"));
+    }
+
+}
