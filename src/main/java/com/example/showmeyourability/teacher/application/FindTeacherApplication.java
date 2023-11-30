d2c4486507712a450a19924c899fb75278a5bd6e
smilejakdu <ash982416@gmail.com>
2023-11-30 19:12:46 +0900

    find Teacher Application refactoring

--- src/main/java/com/example/showmeyourability/teacher/application/FindTeacherApplication.java
+++ src/main/java/com/example/showmeyourability/teacher/application/FindTeacherApplication.java
@@ -1,6 +1,7 @@
 package com.example.showmeyourability.teacher.application;
 
 import com.example.showmeyourability.comments.domain.Comments;
+import com.example.showmeyourability.comments.domain.QComments;
 import com.example.showmeyourability.comments.infrastructure.dto.FindCommentDto.CommentDto;
 import com.example.showmeyourability.shared.Exception.HttpExceptionCustom;
 import com.example.showmeyourability.teacher.domain.QTeacher;
@@ -9,15 +10,14 @@ import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.F
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.FindTeacherResponseDto;
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.TeacherDto;
 import com.example.showmeyourability.teacher.infrastructure.repository.TeacherRepository;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -27,50 +27,35 @@ public class FindTeacherApplication {
     private final TeacherRepository teacherRepository;
 
     @Transactional
-    public FindTeacherResponseDto findAllTeacher(
-            int page,
-            int size
-    ) {
+    public FindTeacherResponseDto findAllTeacher(int page, int size) {
         QTeacher qTeacher = QTeacher.teacher; // Q 클래스 사용
-//        List<Teacher> teachers = teacherRepository.findAll(PageRequest.of(page, size)).getContent();
-        List<Teacher> teachers = queryFactory
-                .selectFrom(qTeacher)
-//                .orderBy(qTeacher.name.asc()) // 예시 정렬 조건
+        QComments qComments = QComments.comments; // Comments에 대한 Q 클래스
+
+        // 교사 정보와 평균 점수를 함께 조회
+        List<TeacherDto> teacherDtos = queryFactory
+                .select(Projections.constructor(TeacherDto.class,
+                        qTeacher.id,
+                        qTeacher.career,
+                        qTeacher.user.email, // Email 필드 추가
+                        qTeacher.skill,
+                        qTeacher.user.id,
+                        qComments.likes.avg().coalesce(0.0) // 평균 점수 계산 및 null 처리
+                ))
+                .from(qTeacher)
+                .leftJoin(qTeacher.comments, qComments)
+                .groupBy(qTeacher.id)
+                .orderBy(qTeacher.user.email.asc())
                 .offset((long) page * size)
                 .limit(size)
                 .fetch();
 
+        // 총 페이지 수 계산
+        long total = queryFactory
+                .selectFrom(qTeacher)
+                .fetchCount();
 
-        HashMap<Integer, Double> hashMap = new HashMap<>();
+        int lastPage = (int) Math.ceil((double) total / size);
 
-        for (Teacher teacher : teachers) {
-            Double sum = 0.0;
-            List<Comments> comments = teacher.getComments();
-            if (comments.isEmpty()) {
-                hashMap.put(teacher.getId().intValue(), 0.0);
-                continue;
-            }
-
-            for (Comments comment : comments) {
-                sum += comment.getLikes();
-            }
-
-            hashMap.put(teacher.getId().intValue(), Math.round(sum / comments.size() * 10) / 10.0);
-        }
-
-        List<TeacherDto> teacherDtos = new ArrayList<>();
-        for (Teacher teacher : teachers) {
-            TeacherDto teacherDto = TeacherDto.builder()
-                    .id(teacher.getId())
-                    .career(teacher.getCareer())
-                    .skill(teacher.getSkill())
-                    .userId(teacher.getUser().getId())
-                    .avgScore(hashMap.get(teacher.getId().intValue()))
-                    .build();
-            teacherDtos.add(teacherDto);
-        }
-
-        int lastPage = teacherRepository.findAll(PageRequest.of(page, size)).getTotalPages();
         return convertToFindTeacherResponseDto(lastPage, teacherDtos);
     }
 
--- src/main/java/com/example/showmeyourability/teacher/infrastructure/dto/FindTeacherDto/TeacherDto.java
+++ src/main/java/com/example/showmeyourability/teacher/infrastructure/dto/FindTeacherDto/TeacherDto.java
@@ -1,36 +1,20 @@
 package com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TeacherDto {
     private Long id;
     private String career;
-
     private String email;
-
     private String skill;
-
     private Long userId;
-
     private Double avgScore;
-
-    @Builder
-    public TeacherDto(
-            Long id,
-            String career,
-            String email,
-            String skill,
-            Long userId,
-            Double avgScore
-            ) {
-        this.id = id;
-        this.career = career;
-        this.email = email;
-        this.skill = skill;
-        this.userId = userId;
-        this.avgScore = avgScore;
-    }
 }
