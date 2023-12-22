69b6fc00a763ebb89d71a20671801908f0299612
smilejakdu <ash982416@gmail.com>
2023-12-22 21:29:30 +0900

    refac:find recent teacher application

--- src/main/java/com/example/showmeyourability/teacher/application/FindRecentTeacherApplication.java
+++ src/main/java/com/example/showmeyourability/teacher/application/FindRecentTeacherApplication.java
@@ -1,24 +1,30 @@
 package com.example.showmeyourability.teacher.application;
 
+import com.example.showmeyourability.teacher.domain.QTeacher;
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.FindRecentTeacherResponseDto;
 import com.example.showmeyourability.teacher.infrastructure.dto.FindTeacherDto.TeacherDto;
-import com.example.showmeyourability.teacher.infrastructure.repository.TeacherRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FindRecentTeacherApplication {
-    private final TeacherRepository teacherRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Transactional
     public FindRecentTeacherResponseDto execute() {
         LocalDateTime fourDaysAgo = LocalDateTime.now().minusDays(4);
-        List<TeacherDto> teacherDtoList = teacherRepository.findByCreatedAtAfter(fourDaysAgo)
+        QTeacher qTeacher = QTeacher.teacher;
+
+        List<TeacherDto> teacherDtoList = queryFactory.selectFrom(qTeacher)
+                .where(qTeacher.createdAt.after(fourDaysAgo))
+                .fetch()
                 .stream()
                 .map(teacher -> TeacherDto.builder()
                         .id(teacher.getId())
@@ -27,7 +33,7 @@ public class FindRecentTeacherApplication {
                         .skill(teacher.getSkill())
                         .userId(teacher.getUser().getId())
                         .build())
-                .toList();
+                .collect(Collectors.toList());
 
         return FindRecentTeacherResponseDto.builder()
                 .teacherDtoList(teacherDtoList)
